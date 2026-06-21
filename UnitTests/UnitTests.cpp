e6b5142a1974ee0a4a86129d397815af55a36e57
dimmazzda <dimmazzda1324@mail.ru>
2026-06-21 18:26:14 +0300

    Реализованы Unit-тесты метода substract Описан тестовый класс substractOfAdjacencyListsTests и его тестировочные методы

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -7,13 +7,479 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
 {
-	TEST_CLASS(UnitTests)
+	TEST_CLASS(substractOfAdjacencyListsTests)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-			Assert::AreEqual(1, 1);
+		TEST_METHOD(partialOverlap)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{1,2},
+				{2},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{1},
+				{},
+				{0}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{2},
+				{2},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(equalLists)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{1},
+				{2},
+				{0}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{1},
+				{2},
+				{0}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{},
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(noOverlap)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 2;
+			minuend.neighbours = {
+				{1},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 2;
+			subtrahend.neighbours = {
+				{},
+				{0}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 2;
+			expected.neighbours = {
+				{1},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(fullOverlapOfSubtrahend)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{1,2,3},
+				{1,2},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{1,2},
+				{2},
+				{}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{3},
+				{1},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(fullOverlapOfMinuend)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 2;
+			minuend.neighbours = {
+				{1,2},
+				{2}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 2;
+			subtrahend.neighbours = {
+				{1,2,3},
+				{2}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 2;
+			expected.neighbours = {
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(emptyMinuend)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{},
+				{},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{1},
+				{2},
+				{}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{},
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(emptySubtrahend)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{1},
+				{2},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{},
+				{},
+				{}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{1},
+				{2},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(diffVertexCntMinSmler)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 2;
+			minuend.neighbours = {
+				{1},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{1},
+				{2},
+				{0}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 2;
+			expected.neighbours = {
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(diffVertexCntMinBger)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{1},
+				{2},
+				{0}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 2;
+			subtrahend.neighbours = {
+				{1},
+				{}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{},
+				{2},
+				{0}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(selfSubtraction)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 2;
+			minuend.neighbours = {
+				{1, 2},
+				{0}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 2;
+			expected.neighbours = {
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(minuend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(duplicateEdges)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 2;
+			minuend.neighbours = {
+				{1, 1, 2},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 2;
+			subtrahend.neighbours = {
+				{1},
+				{}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 2;
+			expected.neighbours = {
+				{1, 2},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(loopEdges)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{0, 1},
+				{2},
+				{}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{0},
+				{2},
+				{}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{1},
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(complexTest1)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{0, 0, 1, 2},
+				{0, 2},
+				{2}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 3;
+			subtrahend.neighbours = {
+				{0, 1},
+				{0, 1, 2},
+				{2}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{0, 2},
+				{},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(complexTest2)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{0, 1, 1},
+				{0, 1, 2},
+				{0}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 2;
+			subtrahend.neighbours = {
+				{1},
+				{1}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{0, 1},
+				{0, 2},
+				{}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
+		}
+
+		TEST_METHOD(complexTest3)
+		{	//уменьшаемое
+			adjacencyList minuend;
+			minuend.countOfVertices = 3;
+			minuend.neighbours = {
+				{1, 0, 0},
+				{2, 0, 1},
+				{0}
+			};
+			//вычитаемое
+			adjacencyList subtrahend;
+			subtrahend.countOfVertices = 2;
+			subtrahend.neighbours = {
+				{0, 1, 1},
+				{1},
+				{1}
+			};
+			//ожидаемый результат
+			adjacencyList expected;
+			expected.countOfVertices = 3;
+			expected.neighbours = {
+				{0, 1},
+				{0, 2},
+				{0}
+			};
+
+			//результат
+			adjacencyList* result = minuend.substract(subtrahend);
+
+			//проверка
+			Assert::IsTrue(result->isEqualTo(expected));
 		}
 	};
 }
