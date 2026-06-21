01e734eea575402544cda3d9ce21801dce127e32
dimmazzda <dimmazzda1324@mail.ru>
2026-06-21 19:14:32 +0300

    Реализована вспомагательная тестировочная функция arraysAreEqual Для удобной  проверки на равенство массивов со степенями заходов реализована функция сравнения массивов

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -512,4 +512,24 @@ namespace UnitTests
 			delete result;
 		}
 	};
+
+	TEST_CLASS(parseGraphFromTextTests)
+	{
+	public:
+
+		bool arraysAreEqual(int* array1, int* array2, int length) {
+			for (int i = 0; i < length; i++)
+			{
+				if (array1[i] != array2[i])
+					return false;
+			}
+			return true;
+		}
+
+		
+
+	};
+
+
 }
+
