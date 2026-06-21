f78440a11247f3f61d447a4c7f43de4f9df51c4d
dimmazzda <dimmazzda1324@mail.ru>
2026-06-21 17:21:24 +0300

    Создан тестировочный проект В решение добавлен и настроен проект машинных модульных тестов

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ivanov_47_knpo/headers.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(1, 1);
+		}
+	};
+}
